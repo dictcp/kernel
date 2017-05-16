d0c5a536f4a70e870fe7e126372c69587a54b900
Jeffery Miller <jmiller@neverware.com>
2017-05-16 15:47:45 -0400

    ALSA: hda - Fix the headphone control order on the Fujitsu Lifebook T731
    The Fujitsu Lifebook T731 has two headphones, one is on the Dock
    and the other is on the device. Change the def association
    such that the built in headphone pin comes before the dock pin.
    This change allows the switch control to be associated with
    the appropriate pin. Without it the switches would not mute
    the appropriate jacks.
    
    OVER-4874

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -4807,6 +4807,7 @@ enum {
 	ALC269_FIXUP_LIFEBOOK,
 	ALC269_FIXUP_LIFEBOOK_EXTMIC,
 	ALC269_FIXUP_LIFEBOOK_HP_PIN,
+	ALC269_FIXUP_LIFEBOOK_HP_PIN_ORDER,
 	ALC269_FIXUP_LIFEBOOK_NO_HP_TO_LINEOUT,
 	ALC269_FIXUP_AMIC,
 	ALC269_FIXUP_DMIC,
@@ -4991,6 +4992,15 @@ static const struct hda_fixup alc269_fixups[] = {
 		.type = HDA_FIXUP_PINS,
 		.v.pins = (const struct hda_pintbl[]) {
 			{ 0x21, 0x0221102f }, /* HP out */
+			{ }
+		},
+	},
+	[ALC269_FIXUP_LIFEBOOK_HP_PIN_ORDER] = {
+		.type = HDA_FIXUP_PINS,
+		/* Make sure the builtin HP is first. */
+		.v.pins = (const struct hda_pintbl[]) {
+			{ 0x21, 0x0221101f }, /* HP out */
+			{ 0x1a, 0x2221102f }, /* HP dock out */
 			{ }
 		},
 	},
@@ -5706,7 +5716,7 @@ static const struct snd_pci_quirk alc269_fixup_tbl[] = {
 	SND_PCI_QUIRK(0x104d, 0x9099, "Sony VAIO S13", ALC275_FIXUP_SONY_DISABLE_AAMIX),
 	SND_PCI_QUIRK(0x10cf, 0x1475, "Lifebook", ALC269_FIXUP_LIFEBOOK),
 	SND_PCI_QUIRK(0x10cf, 0x159f, "Lifebook E780", ALC269_FIXUP_LIFEBOOK_NO_HP_TO_LINEOUT),
-	SND_PCI_QUIRK(0x10cf, 0x15dc, "Lifebook T731", ALC269_FIXUP_LIFEBOOK_HP_PIN),
+	SND_PCI_QUIRK(0x10cf, 0x15dc, "Lifebook T731", ALC269_FIXUP_LIFEBOOK_HP_PIN_ORDER),
 	SND_PCI_QUIRK(0x10cf, 0x1757, "Lifebook E752", ALC269_FIXUP_LIFEBOOK_HP_PIN),
 	SND_PCI_QUIRK(0x10cf, 0x1845, "Lifebook U904", ALC269_FIXUP_LIFEBOOK_EXTMIC),
 	SND_PCI_QUIRK(0x144d, 0xc109, "Samsung Ativ book 9 (NP900X3G)", ALC269_FIXUP_INV_DMIC),
