20fba006bb5d94e705acfb282ea7cb631c5da4b5
Jeffery Miller <jmiller@neverware.com>
2018-03-30 14:01:25 -0500

    Revert "tpm_tis: Move ilb_base_addr to tpm_tis_data"
    This reverts commit ed331ebc2422c8827c2079c4f8597d14cdfaa6ea.
    
    The upstream fix will be merged in with v4.14.31.
    Reverting before the merge makes the merge complete cleanly.
    OVER-6316

--- drivers/char/tpm/tpm_tis_core.c
+++ drivers/char/tpm/tpm_tis_core.c
@@ -737,13 +737,6 @@ int tpm_tis_core_init(struct device *dev, struct tpm_tis_data *priv, int irq,
 	priv->phy_ops = phy_ops;
 	dev_set_drvdata(&chip->dev, priv);
 
-	if (is_bsw()) {
-		priv->ilb_base_addr = ioremap(INTEL_LEGACY_BLK_BASE_ADDR,
-					ILB_REMAP_SIZE);
-		if (!priv->ilb_base_addr)
-			return -ENOMEM;
-	}
-
 	if (wait_startup(chip, 0) != 0) {
 		rc = -ENODEV;
 		goto out_err;
@@ -845,16 +838,9 @@ int tpm_tis_core_init(struct device *dev, struct tpm_tis_data *priv, int irq,
 		}
 	}
 
-	rc = tpm_chip_register(chip);
-	if (rc && is_bsw())
-		iounmap(priv->ilb_base_addr);
-
-	return rc;
+	return tpm_chip_register(chip);
 out_err:
 	tpm_tis_remove(chip);
-	if (is_bsw())
-		iounmap(priv->ilb_base_addr);
-
 	return rc;
 }
 EXPORT_SYMBOL_GPL(tpm_tis_core_init);
