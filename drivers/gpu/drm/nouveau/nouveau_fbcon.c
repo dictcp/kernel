25e8c9ad577980426433c4fef9f3c49ae7cc8b20
Jeffery Miller <jmiller@neverware.com>
2018-07-04 14:12:01 -0500

    drm/nouveau/fbcon: Fix NULL pointer access in nouveau_fbcon_destroy.
    It is possible for this to get called with a null helper.fb.
    This can cause a panic when unloading the nouveau module on
    a Optimus system.
    
    Upstream master commit 34112bf4935d ("drm/nouveau/fbcon: fix NULL
    pointer access in nouveau_fbcon_destroy") includes part of the fix of
    this patch.
    
    OVER-5961

--- drivers/gpu/drm/nouveau/nouveau_fbcon.c
+++ drivers/gpu/drm/nouveau/nouveau_fbcon.c
@@ -423,12 +423,15 @@ nouveau_fbcon_output_poll_changed(struct drm_device *dev)
 static int
 nouveau_fbcon_destroy(struct drm_device *dev, struct nouveau_fbdev *fbcon)
 {
-	struct nouveau_framebuffer *nouveau_fb = nouveau_framebuffer(fbcon->helper.fb);
+	struct nouveau_framebuffer *nouveau_fb = NULL;
+
+	if (fbcon->helper.fb)
+		nouveau_fb = nouveau_framebuffer(fbcon->helper.fb);
 
 	drm_fb_helper_unregister_fbi(&fbcon->helper);
 	drm_fb_helper_fini(&fbcon->helper);
 
-	if (nouveau_fb->nvbo) {
+	if (nouveau_fb && nouveau_fb->nvbo) {
 		nouveau_bo_vma_del(nouveau_fb->nvbo, &nouveau_fb->vma);
 		nouveau_bo_unmap(nouveau_fb->nvbo);
 		nouveau_bo_unpin(nouveau_fb->nvbo);
