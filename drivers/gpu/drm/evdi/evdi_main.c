moetayuko/CAF_kernel_msm-3.18

CHROMIUM: drm/evdi: Add Evdi module
Based on module from kernel 3.18

BUG=None
TEST=Build on samus with kernel 4.4

Change-Id: I04332fe479d84bb16d7fe0cbca8a69503d22405a
Signed-off-by: Tadeusz Marquardt <tadeusz.marquardt@displaylink.com>
Reviewed-on: https://chromium-review.googlesource.com/386616
Commit-Ready: Bernie Thompson <bhthompson@chromium.org>
Tested-by: Bernie Thompson <bhthompson@chromium.org>
Reviewed-by: Bernie Thompson <bhthompson@chromium.org>

[rebase49(groeck): Squashed all evdi commits:
	CHROMIUM: drm/evdi: Adjust Evdi module to kernel 4.4
	CHROMIUM: drm/evdi: Fix build error due to bad indentation
	CHROMIUM: drm/evdi: Set correct evdi events size
	CHROMIUM: drm/evdi: Implement create_handle function for framebuffers
	CHROMIUM: drm/evdi: add 32-bit ioctl compatibility routines.
	CHROMIUM: drm/evdi: flush physical pages after allocating
	CHROMIUM: drm/evdi: Always send mode change event
	CHROMIUM: drm/evdi: Filter unsupported display modes
	CHROMIUM: drm/evdi: detect evdi's own gem object while importing
    kernel API changes:
	drm_gem_object_lookup() parameters changed
	drm_connector_unplug_all() no longer required
	dirty_info_property dropped upstream
	drm_mode_create_dirty_info_property() no longer exists ]
Signed-off-by: Guenter Roeck <groeck@chromium.org>

drivers/gpu/drm/evdi/evdi_main.c
@@ -1,0 +1,101 @@
+/*
+ * Copyright (C) 2012 Red Hat
+ * Copyright (c) 2015 - 2016 DisplayLink (UK) Ltd.
+ *
+ * Based on parts on udlfb.c:
+ * Copyright (C) 2009 its respective authors
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License v2. See the file COPYING in the main directory of this archive for
+ * more details.
+ */
+
+#include <linux/platform_device.h>
+#include <drm/drmP.h>
+#include "evdi_drv.h"
+#include "evdi_cursor.h"
+
+int evdi_driver_load(struct drm_device *dev, unsigned long flags)
+{
+	struct platform_device *platdev = NULL;
+	struct evdi_device *evdi;
+	int ret;
+
+	EVDI_CHECKPT();
+	evdi = kzalloc(sizeof(struct evdi_device), GFP_KERNEL);
+	if (!evdi)
+		return -ENOMEM;
+
+	evdi->ddev = dev;
+	dev->dev_private = evdi;
+
+	ret =  evdi_cursor_alloc(&evdi->cursor);
+	if (ret)
+		goto err;
+
+	EVDI_CHECKPT();
+	ret = evdi_modeset_init(dev);
+
+	if (ret)
+		goto err;
+
+	ret = evdi_fbdev_init(dev);
+	if (ret)
+		goto err;
+
+	ret = drm_vblank_init(dev, 1);
+	if (ret)
+		goto err_fb;
+
+	ret = evdi_painter_init(evdi);
+	if (ret)
+		goto err_fb;
+
+	evdi_stats_init(evdi);
+
+	drm_kms_helper_poll_init(dev);
+
+	platdev = to_platform_device(dev->dev);
+	platform_set_drvdata(platdev, dev);
+
+	return 0;
+
+err_fb:
+	evdi_fbdev_cleanup(dev);
+err:
+	kfree(evdi);
+	EVDI_ERROR("%d\n", ret);
+	if (evdi->cursor)
+		evdi_cursor_free(evdi->cursor);
+	return ret;
+}
+
+int evdi_driver_unload(struct drm_device *dev)
+{
+	struct evdi_device *evdi = dev->dev_private;
+
+	EVDI_CHECKPT();
+
+	drm_vblank_cleanup(dev);
+	drm_kms_helper_poll_fini(dev);
+	evdi_fbdev_unplug(dev);
+	if (evdi->cursor)
+		evdi_cursor_free(evdi->cursor);
+	evdi_painter_cleanup(evdi);
+	evdi_stats_cleanup(evdi);
+	evdi_fbdev_cleanup(dev);
+	evdi_modeset_cleanup(dev);
+
+	kfree(evdi);
+	return 0;
+}
+
+void evdi_driver_preclose(struct drm_device *drm_dev, struct drm_file *file)
+{
+	struct evdi_device *evdi = drm_dev->dev_private;
+
+	EVDI_CHECKPT();
+	if (evdi)
+		evdi_painter_close(evdi, file);
+}
+

dictcp/kernel

[rebase412(groeck): Re-import latest version from upstream:
	https://github.com/DisplayLink/evdi.git]
re-import
Change-Id: I59e66e323f40b37d86a31855d15b387090a78fd7
(cherry picked from commit ab168fc5462f33ce1ec40509696497b488741b3c)
OVER-4709

drivers/gpu/drm/evdi/evdi_main.c
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2012 Red Hat
+ * Copyright (c) 2015 - 2016 DisplayLink (UK) Ltd.
+ *
+ * Based on parts on udlfb.c:
+ * Copyright (C) 2009 its respective authors
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License v2. See the file COPYING in the main directory of this archive for
+ * more details.
+ */
+
+#include <linux/platform_device.h>
+#include <drm/drmP.h>
+#include "evdi_drv.h"
+#include "evdi_cursor.h"
+
+int evdi_driver_setup_early(struct drm_device *dev)
+{
+	struct platform_device *platdev = NULL;
+	struct evdi_device *evdi;
+	int ret;
+
+	EVDI_CHECKPT();
+	evdi = kzalloc(sizeof(struct evdi_device), GFP_KERNEL);
+	if (!evdi)
+		return -ENOMEM;
+
+	evdi->ddev = dev;
+	dev->dev_private = evdi;
+
+	ret =  evdi_cursor_alloc(&evdi->cursor);
+	if (ret)
+		goto err;
+
+	EVDI_CHECKPT();
+	ret = evdi_modeset_init(dev);
+
+	if (ret)
+		goto err;
+
+	ret = evdi_fbdev_init(dev);
+	if (ret)
+		goto err;
+
+	ret = drm_vblank_init(dev, 1);
+	if (ret)
+		goto err_fb;
+
+	ret = evdi_painter_init(evdi);
+	if (ret)
+		goto err_fb;
+
+	drm_kms_helper_poll_init(dev);
+
+	platdev = to_platform_device(dev->dev);
+	platform_set_drvdata(platdev, dev);
+
+	return 0;
+
+err_fb:
+	evdi_fbdev_cleanup(dev);
+err:
+	kfree(evdi);
+	EVDI_ERROR("%d\n", ret);
+	if (evdi->cursor)
+		evdi_cursor_free(evdi->cursor);
+	return ret;
+}
+
+void evdi_driver_setup_late(struct drm_device *dev)
+{
+	evdi_stats_init(dev->dev_private);
+}
+
+void evdi_driver_unload(struct drm_device *dev)
+{
+	struct evdi_device *evdi = dev->dev_private;
+
+	EVDI_CHECKPT();
+
+	drm_vblank_cleanup(dev);
+	drm_kms_helper_poll_fini(dev);
+	evdi_fbdev_unplug(dev);
+	if (evdi->cursor)
+		evdi_cursor_free(evdi->cursor);
+	evdi_painter_cleanup(evdi);
+	evdi_stats_cleanup(evdi);
+	evdi_fbdev_cleanup(dev);
+	evdi_modeset_cleanup(dev);
+
+	kfree(evdi);
+}
+
+void evdi_driver_preclose(struct drm_device *drm_dev, struct drm_file *file)
+{
+	struct evdi_device *evdi = drm_dev->dev_private;
+
+	EVDI_CHECKPT();
+	if (evdi)
+		evdi_painter_close(evdi, file);
+}
+