9d59ed3f15acb1531c09efe53f06313144d529b2
Jeffery Miller <jmiller@neverware.com>
2019-07-29 15:37:35 -0500

    evdi: Remove drm_encoder_helper_add
    These stubs don't seem to be needed for the 4.19 kernel.

--- drivers/gpu/drm/evdi/evdi_encoder.c
+++ drivers/gpu/drm/evdi/evdi_encoder.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (C) 2012 Red Hat
- * Copyright (c) 2015 - 2016 DisplayLink (UK) Ltd.
+ * Copyright (c) 2015 - 2018 DisplayLink (UK) Ltd.
  *
  * Based on parts on udlfb.c:
  * Copyright (C) 2009 its respective authors
@@ -23,48 +23,6 @@ static void evdi_enc_destroy(struct drm_encoder *encoder)
 	kfree(encoder);
 }
 
-static void evdi_encoder_disable(__always_unused struct drm_encoder *encoder)
-{
-}
-
-static bool evdi_mode_fixup(
-			__always_unused struct drm_encoder *encoder,
-			__always_unused const struct drm_display_mode *mode,
-			__always_unused struct drm_display_mode *adjusted_mode)
-{
-	return true;
-}
-
-static void evdi_encoder_prepare(__always_unused struct drm_encoder *encoder)
-{
-}
-
-static void evdi_encoder_commit(__always_unused struct drm_encoder *encoder)
-{
-}
-
-static void evdi_encoder_mode_set(
-			__always_unused struct drm_encoder *encoder,
-			__always_unused struct drm_display_mode *mode,
-			__always_unused struct drm_display_mode *adjusted_mode)
-{
-}
-
-static void evdi_encoder_dpms(
-			__always_unused struct drm_encoder *encoder,
-			__always_unused int mode)
-{
-}
-
-static const struct drm_encoder_helper_funcs evdi_helper_funcs = {
-	.dpms = evdi_encoder_dpms,
-	.mode_fixup = evdi_mode_fixup,
-	.prepare = evdi_encoder_prepare,
-	.mode_set = evdi_encoder_mode_set,
-	.commit = evdi_encoder_commit,
-	.disable = evdi_encoder_disable,
-};
-
 static const struct drm_encoder_funcs evdi_enc_funcs = {
 	.destroy = evdi_enc_destroy,
 };
@@ -85,7 +43,6 @@ struct drm_encoder *evdi_encoder_init(struct drm_device *dev)
 		goto err_encoder;
 	}
 
-	drm_encoder_helper_add(encoder, &evdi_helper_funcs);
 	encoder->possible_crtcs = 1;
 	return encoder;
 
