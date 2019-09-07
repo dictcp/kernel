7860d6b03026ba4b77cd2d6d6c35da356f6dfeef
Jeffery Miller <jmiller@neverware.com>
2019-09-06 23:57:07 -0500

    Avoid EFI runtime services on HP ProBook x360 N3450.
    HP ProBook x360 11 G1 EE with the Celeron N3450 will
    panic when efi variables are set.
    Disable EFI services on these machines to protect against
    this crash.
    The product name is shared with other devices
    but the Celeron N3450 variant can be identified
    with the SKU.
    
    OVER-9289

--- init/main.c
+++ init/main.c
@@ -530,6 +530,32 @@ static void __init mm_init(void)
 	pti_init();
 }
 
+static const struct dmi_system_id __initconst dmi_system_efi_blacklist[] = {
+	{
+		/* OVER-6986 Runtime services can panic on the T430. */
+		.matches = {
+			DMI_EXACT_MATCH(DMI_PRODUCT_FAMILY, "ThinkPad T430"),
+		},
+	},
+	{
+		/* HP ProBook x360 11 G1 EE with the Celeron N3450 will
+		 * panic  when efi variables are set.
+		 * Disable EFI services on these machines to protect against
+		 * the problem.
+		 * The product name is shared with other devices
+		 * but the Celeron N3450 variant can be identified
+		 * with the SKU.
+		 * OVER-9289
+		 */
+		.matches = {
+			DMI_EXACT_MATCH(DMI_SYS_VENDOR, "HP"),
+			DMI_EXACT_MATCH(DMI_PRODUCT_SKU, "2QU86UT#ABL"),
+		},
+	},
+	{}
+
+};
+
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
@@ -705,7 +731,7 @@ asmlinkage __visible void __init start_kernel(void)
 #ifdef CONFIG_X86
 	/* OVER-6986 Runtime services can panic on the T430. */
 	if (efi_enabled(EFI_RUNTIME_SERVICES) &&
-			dmi_match(DMI_PRODUCT_FAMILY, "ThinkPad T430"))
+			dmi_check_system(dmi_system_efi_blacklist))
 	{
 		pr_info("EFI: Disable runtime services on buggy firmware.");
 		clear_bit(EFI_RUNTIME_SERVICES, &efi.flags);
