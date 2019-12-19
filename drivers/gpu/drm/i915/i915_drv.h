9b7885af601fb9faddd7c601157c831de0c7c212
Nicholas Bishop <nbishop@neverware.com>
2019-12-19 13:54:34 -0500

    neverware: fix hangs on Intel compute stick
    This patch comes from here:
    https://github.com/ouija/android-x86_insignia_flex8/commit/776c9758aad7770ed09461d821a3d0890b0426da#diff-70cc79a7d0e74d6b8e938ad9383a2192
    
    It purports to be a backport of the upstream commit
    a75d035fedbdecf83f86767aa2e4d05c8c4ffd95. The commit message for the
    upstream message follows:
    
    ---
    
        drm/i915: Disable preemption and sleeping while using the punit sideband
    
        While we talk to the punit over its sideband, we need to prevent the cpu
        from sleeping in order to prevent a potential machine hang.
    
        Note that by itself, it appears that pm_qos_update_request (via
        intel_idle) doesn't provide a sufficient barrier to ensure that all core
        are indeed awake (out of Cstate) and that the package is awake. To do so,
        we need to supplement the pm_qos with a manual ping on_each_cpu.
    
        v2: Restrict the heavy-weight wakeup to just the ISOF_PORT_PUNIT, there
        is insufficient evidence to implicate a wider problem atm. Similarly,
        restrict the w/a to Valleyview, as Cherryview doesn't have an angry cadre
        of users.
    
        The working theory, courtesy of Ville and Hans, is the issue lies within
        the power delivery and so is likely to be unit and board specific and
        occurs when both the unit/fw require extra power at the same time as the
        cpu package is changing its own power state.
    
        References: https://bugzilla.kernel.org/show_bug.cgi?id=109051
        References: https://bugs.freedesktop.org/show_bug.cgi?id=102657
        References: https://bugzilla.kernel.org/show_bug.cgi?id=195255
        Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
        Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
        Cc: Hans de Goede <hdegoede@redhat.com>
        Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
        Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
        Link: https://patchwork.freedesktop.org/patch/msgid/20190426081725.31217-1-chris@chris-wilson.co.uk
    
    ---
    
    https://neverware.atlassian.net/browse/OVER-10996

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1689,6 +1689,7 @@ struct drm_i915_private {
 
 	/* Sideband mailbox protection */
 	struct mutex sb_lock;
+	struct pm_qos_request sb_qos;
 
 	/** Cached value of IMR to avoid reads in updating the bitfield */
 	union {
